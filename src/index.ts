4a08dcb619916706ee8281ef0d3eb457d82fe28b
nofamex <nsiren517@gmail.com>
2021-08-01 11:45:44 +0700

    feat: add husky

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 const app = fastify({ logger: true });
 
 app.get("/", async (request, reply) => {
-  return { hello: "world" };
+  return { hello: "opal" };
 });
 
 const start = async () => {
