0f4e90f6829a1fd1395c1078f65e19dbe8ed5623
nofamex <nsiren517@gmail.com>
2021-08-01 12:21:38 +0700

    fix eslint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify({ logger: true });
 
-app.get("/", async (request, reply) => {
-  return { hello: "world" };
+app.get('/', async (request, reply) => {
+  return { hello: 'world' };
 });
 
 const start = async () => {
   try {
-    await app.listen(8080, "0.0.0.0");
+    await app.listen(8080, '0.0.0.0');
   } catch (err) {
     app.log.error(err);
     process.exit(1);
