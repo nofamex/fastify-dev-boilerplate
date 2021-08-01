009f66f23d29ecccf4cce44d253f6ed65196a993
nofamex <nsiren517@gmail.com>
2021-08-01 10:53:49 +0700

    initial init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+const app = fastify({ logger: true });
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await app.listen(5000);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
